10f456a6e483e79996ace585d64162c45a25ca91
Elias Dorneles <eliasdorneles@gmail.com>
2015-05-01 22:55:57 -0300

    pulling more functions to upper level for cleaner API

--- skinfer/__init__.py
+++ skinfer/__init__.py
@@ -6,4 +6,6 @@ __author__ = 'Scrapinghub'
 __email__ = 'info@scrapinghub.com'
 __version__ = '0.1.0'
 
-from skinfer.json_schema_merger import merge_schema  # NOQA
+from .json_schema_merger import merge_schema  # NOQA
+from .schema_inferer import generate_schema_for_sample as generate_schema  # NOQA
+from .schema_inferer import generate_and_merge_schemas as infer_schema  # NOQA
